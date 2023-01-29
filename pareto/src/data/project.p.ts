869f2b29a176d60d610c2d77f54daaec261a35e1
corno <corno.astn.a@schraverus.com>
2023-01-30 00:57:06 +0100

    .

--- pareto/src/data/project.p.ts
+++ pareto/src/data/project.p.ts
@@ -7,6 +7,12 @@ const d = pr.wrapRawDictionary
 import { $ as api } from "./api.p"
 
 export const $: mproject.TProject = {
+    'name': "lib-analyse-path",
+
+    'author': "Corno",
+    'description': "given a provided directory structure definition, this library analyses a file path, indicates if it conforms to the expected structure, and provides the applicable pattern for that file path. for example: if you provide a definition and a path like 'customers/John/contactinfo.txt', then the provided pattern will be 'customers/*/*.txt ",
+    'license': "ISC",
+
     'type': ['library', {}],
     'modules': d({
         "main": {
@@ -14,5 +20,14 @@ export const $: mproject.TProject = {
 
         },
     }),
-    'main': "main"
+    'main': "main",
+    'pubdependencies': d({
+        "res-pareto-path": {},
+        "glo-pareto-common": {},
+        "res-pareto-tostring": {},
+    }),
+    'testdependencies': d({
+        "res-pareto-path": {},
+        "res-pareto-tostring": {},
+    }),
 }
