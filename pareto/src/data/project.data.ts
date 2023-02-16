81d867165606e4e589048f9505956a2e8aa1d3d0
corno <corno.astn.a@schraverus.com>
2023-02-16 19:47:07 +0100

    .

--- pareto/src/data/project.data.ts
+++ pareto/src/data/project.data.ts
@@ -11,7 +11,7 @@ export const $: mproject.T.Project = {
     'description': "given a provided directory structure definition, this library analyses a file path, indicates if it conforms to the expected structure, and provides the applicable pattern for that file path. for example: if you provide a definition and a path like 'customers/John/contactinfo.txt', then the provided pattern will be 'customers/*/*.txt ",
     'license': "ISC",
 
-    'pubdependencies': d({
+    'dependencies': d({
         "res-pareto-path": {},
         "glo-pareto-common": {},
         "res-pareto-tostring": {},
@@ -28,6 +28,13 @@ export const $: mproject.T.Project = {
                 "res-pareto-path": {},
                 "res-pareto-tostring": {},
             }),
+            'glossary': {
+                'functions': d({}),
+                'imports': d({}),
+                'parameters': d({}),
+                'types': d({}),
+                'interfaces': d({}),
+            },
         }
     }],
 }
